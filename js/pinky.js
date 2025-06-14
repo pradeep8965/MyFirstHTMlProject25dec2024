3eb2988b98980380f6ea2ccbe6a9f555651e8bb9
“misterpp143@gmail.comgit config --global user.email misterpp143@gmail.comgit config  credential.helper ‘store’git config --global user.name “misterpp143@gmail.com <misterpp143@gmail.com>
2025-06-14 22:49:42 +0530

    a new project

---
+++ js/pinky.js
@@ -0,0 +1,16 @@
+
+let p = document.querySelector("p");
+p.style.backgroundColor = "blue";
+p.style.color = "white";
+
+
+let h2 = document.querySelector("h2");
+h2.style.fontSize = "20px";  
+h2.style.backgroundColor = "red";  
+h2.style.color = "white";  
+
+
+let h1 = document.querySelector("h1");
+console.dir(h1.innerText);
+h1.innerText = h1.innerText + " pinky "; 
+
--- pinky2.js
+++
@@ -1,8 +0,0 @@
-
-
-function cube() 
-{
-    var a=document.getElementById("5").value;
-    document.write("cube of" +5 +"=" +5*5*5);
-}
-
